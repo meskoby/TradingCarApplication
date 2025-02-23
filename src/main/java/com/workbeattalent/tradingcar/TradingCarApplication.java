a233703d418eaaaa155abb27c86374ad2dcc4ce4
Messack KOBE <hkobe.messack@gmail.com>
2025-02-23 16:18:23 +0100

    initial commit

---
+++ src/main/java/com/workbeattalent/tradingcar/TradingCarApplication.java
@@ -0,0 +1,13 @@
+package com.workbeattalent.tradingcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workbeattalent/tradingcar/TradingCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workbeattalent.tradingcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
