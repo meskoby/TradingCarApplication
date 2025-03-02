37011d378a839ccda0d625397587ee62d2789ef9
leonel ka <waboleonel@gmail.com>
2025-03-03 00:07:18 +0100

    Code refactoring, remove unused imports

--- src/main/java/com/workbeattalent/tradingcar/model/Client.java
+++ src/main/java/com/workbeattalent/tradingcar/model/Client.java
@@ -7,13 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-//@NoArgsConstructor
 @AllArgsConstructor
 public class Client {
     @Id
